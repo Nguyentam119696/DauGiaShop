1580e0e5917622df6b258149572801bfdd527496
Nguyentam119696 <nguyentam2112015@gmail.com>
2017-06-17 15:11:28 +0700

    upload project first time

---
+++ DoAn_WebShop/DoAn_WebShop/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="DoAn_WebShop.MvcApplication" Language="C#" %>
---
+++ DoAn_WebShop/DoAn_WebShop/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(DoAn_WebShop.Startup))]
+namespace DoAn_WebShop
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
